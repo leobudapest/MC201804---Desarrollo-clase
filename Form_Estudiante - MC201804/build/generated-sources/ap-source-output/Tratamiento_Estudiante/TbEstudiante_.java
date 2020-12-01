5fb67d8fe13f1e755f4f199fd3f369e27262433f
leobudapest <56747005+leobudapest@users.noreply.github.com>
2020-11-30 23:21:37 -0600

    Initial commit

---
+++ Form_Estudiante - MC201804/build/generated-sources/ap-source-output/Tratamiento_Estudiante/TbEstudiante_.java
@@ -0,0 +1,16 @@
+package Tratamiento_Estudiante;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-30T23:17:32")
+@StaticMetamodel(TbEstudiante.class)
+public class TbEstudiante_ { 
+
+    public static volatile SingularAttribute<TbEstudiante, String> seccion;
+    public static volatile SingularAttribute<TbEstudiante, Integer> id;
+    public static volatile SingularAttribute<TbEstudiante, String> nombre;
+    public static volatile SingularAttribute<TbEstudiante, String> email;
+
+}
